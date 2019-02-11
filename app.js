f2a633378ceebdf77ac4602c9d3d57bba408c89b
Puneet <birmaan.puneet19@gmail.com>
2019-02-11 17:16:59 +0530

    mongoDB connection and setup done
    Signed-off-by: Puneet <birmaan.puneet19@gmail.com>

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// requiring all the dependencies
 const express = require('express')
 const bodyParser = require('body-parser')
 const passport = require('passport')
@@ -10,22 +11,33 @@ const mongoose = require('mongoose')
 const path = require('path')
 
 
+// mongoDB configuration : 
+const db = require('./setup/myurl').mongoURL;     
+// db contains the mongoURL present in the ./setup/myurl.js
+
+// attempt to connect to database and use promise to handle the situations in 
+// the connection to tht db fails...  => using .then().catch()
+mongoose
+        .connect(db)
+        .then(()=> console.log('db connected'))
+        .catch(err => console.log(err)) 
 
 
 
+// app setup and port declaration
 const app = express()
 const port = process.env.PORT || 3000;
 
 
 
-
+// home route
 app.get('/',(req,res)=>{
-    res.send('This is the home page')
+    res.send('This is the home page of big stack')
 })
 
 
 
-
+// listening app at port
 app.listen(port,()=>{
     console.log(`Server is up and running at port : ${port}`)
 })
---
+++ setup/myurl.js
@@ -0,0 +1,4 @@
+module.exports = { 
+    mongoURL : "mongodb://graspitupguy:Pk4953Pk4953mlab@ds129625.mlab.com:29625/db4bigstack",
+    secret : "myStrongSecret",
+};
