bc5dc0bb3e46cb7f2bfac2c4c7f919d67f433bff
Puneet <birmaan.puneet19@gmail.com>
2019-02-11 22:40:26 +0530

    registration route added + minor changes
    Signed-off-by: Puneet <birmaan.puneet19@gmail.com>

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,4 +1,10 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
+const  JSONwebToken  = require('jsonwebtoken')
+const passport = require('passport')
+
+const key = require('../../setup/myurl') //  the secret in myurl.js
+
 const router = express.Router()
 
 //  at api/auth -> this get request will be served
@@ -7,7 +13,23 @@ const router = express.Router()
 // @desc    -  just for testing
 // @access  -  PUBLIC
 router.get('/', (req,res)=>{
-    res.json({test : "Auth is success"})
+    res.json({test : "Auth is being tested"})
 })
+
+// Import Schema for person to register 
+const Person = require('../../models/Persons')
+
+
+// @type    -  POST
+// @route   -  /api/auth/register
+// @desc    -  route for registration of user
+// @access  -  PUBLIC
+router.post('/register',(req,res)=>{
+    res.send('registration takes here')
+    Person.findOne({email : req.body.email})
+                                           .then()
+                                           .catch(err => console.log(err));
+})
+
 
 module.exports = router ;
