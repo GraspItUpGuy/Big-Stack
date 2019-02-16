b98666ea644f1d015c1cd7348329ab3e8fe9b1b9
Puneet <birmaan.puneet19@gmail.com>
2019-02-16 10:29:45 +0530

    update the profile values and save them
    Signed-off-by: Puneet <birmaan.puneet19@gmail.com>

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -39,12 +39,39 @@ router.post('/',passport.authenticate('jwt', {session : false}),(req,res)=>{
    if(req.body.country)   { profileValues.country =req.body.country  }
    if(req.body.languages != undefiend){
        profileValues.languages = req.body.languages.split(',')
-    } // languages to be comming seperated on basis of ',', hence splitting them
+    } // languages to be coming seperated on basis of ',' , hence splitting them
     if(req.body.youtube)   { profileValues.youtube =req.body.youtube  }
     if(req.body.facebook)   { profileValues.facebook =req.body.facebook  }
     if(req.body.instagram)   { profileValues.instagram =req.body.instagram  }
     if(req.body.github)   { profileValues.github =req.body.github  }
     if(req.body.linkedin)   { profileValues.linkedin =req.body.linkedin}
+ 
+    // data collection done...
+
+    // Database stuff
+    Profile.findOne({user : req.user.id})
+          .then(profile => {
+              if(profile){
+                  Profile.findOneAndUpdate(
+                      { user : req.user.id},
+                      { $set : profileValues},
+                      { new  : true},
+                  ).then(
+                      profile => res.json(profile) 
+                  ).catch(console.log('Problem in updating => profile.js'))
+              } else {
+                  Profile.findOne({username : profileValues.username})
+                        .then(profile => { 
+                            if(profile){
+                                res.status(400).json({username : 'username already esists'})
+                            }
+                        })
+                        .catch( console.log('error ocurred => //database stuff => profile.js'))
+              }
+          } )
+          .catch(console.log('problem in fetching profile => profile.js =>// database stuff'))
+          
+          
 
 })
 
